b6b8de8c384e09b4645c86f62d4b9aa528718d3a
Nikoo <nikoomoallemi@gmail.com>
2025-06-16 03:11:25 +0330

    initial 2 problem with finding loop commit

--- transactions.py
+++ transactions.py
@@ -1,17 +1,20 @@
-TransactionList = input("").split(",")
-count =0
-TCount =0
-loop=1
-for i in range TransactionList.len:
-    ThisTInfo=TransactionList[i].split("")
-    
-    if(int(ThisTInfo[1])>TCount):
-        TCount = int(ThisTInfo[1])
-    for j in range TransactionList.len:
-        compareTInfo =TransactionList[j].split("")
-        if(ThisTInfo[2] == compareTInfo[2] and ThisTInfo[1] == compareTInfo[1]):
-            continue
-        elif(ThisTInfo[2] == compareTInfo[2] and ThisTInfo[1] != compareTInfo[1] and (ThisTInfo[0] =='w' or compareTInfo[0] == 'w')):
-            # ThisTInfo[0] or compareTInfo[0] the one that came first has a flash to the other one
-    # if loop than loop=0
-print(TCount,\n loop)
+TransactionList = input().split(",")
+TCount = 0
+conflict = False
+
+for t in TransactionList:
+    trans_id = int(t[1])
+    if trans_id > TCount:
+        TCount = trans_id
+
+for i in range(len(TransactionList)):
+    op1, tid1, data1 = TransactionList[i][0], TransactionList[i][1], TransactionList[i][2]
+    for j in range(i + 1, len(TransactionList)):
+        op2, tid2, data2 = TransactionList[j][0], TransactionList[j][1], TransactionList[j][2]
+        
+        if tid1 != tid2 and data1 == data2:
+            if op1 == 'w' or op2 == 'w':
+                conflict = True
+
+print(TCount)
+print(0 if conflict else 1)
